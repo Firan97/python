fc0f92f0c3672341f23370d039cb9d8e4e7a7f7f
Firan <firan1997@gmail.com>
2020-11-30 16:29:26 +0300

    asdasd

---
+++ 5-4.py
@@ -0,0 +1,4 @@
+ru_dic = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+with open("text_44.txt", "w", encoding='utf-8') as new_file:
+    with open("text_4.txt", encoding='utf-8') as my_file:
+            new_file.writelines([line.replace(line.split()[0], ru_dic.get(line.split()[0])) for line in my_file])
---
+++ 5-6.py
@@ -0,0 +1,7 @@
+mydict = {}
+with open("text_6.txt", encoding="utf-8") as fobj:
+    for line in fobj:
+        name, stats = line.split(':')
+        name_sum = sum(map(int, "".join([i for i in stats if i == ' ' or '9' >= i >= '0']).split()))
+        mydict[name] = name_sum
+        print(f"{mydict}")
